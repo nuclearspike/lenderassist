c26d7bd3a01428120f7f56bfb2e2662109f968e9
Paul Ericksen <liquidmonkey@gmail.com>
2015-04-28 19:36:56 -0600

    manifest changes

--- inject/live.js
+++ inject/live.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
+    $("#siteNavAboutAnchor").parent().removeClass("urHere");
+    $("#siteNavLiveAnchor").parent().addClass("urHere");
     $("#newLenders").html('0'); //starts out blank and looks like it's an error.
 });
-$("#siteNavAboutAnchor").parent().removeClass("urHere");
-$("#siteNavLiveAnchor").parent().addClass("urHere");
